66b6fa0a430bef03bab21d846c5fd59e71c14839
rbrin <rbrinson2@gmail.com>
2025-06-03 15:31:23 -0400

    SMM0 working and tested for MM

--- src/SMM0.v
+++ src/SMM0.v
@@ -6,7 +6,8 @@ module SMM0
 )    
 (
     input clk, rst,
-    input [DATAWIDTH - 1:0] A, B,
+    input signed [DATAWIDTH - 1:0] A, B,
+    input load,
 
     output reg [DATAWIDTH - 1 :0] C_out
 
@@ -14,36 +15,42 @@ module SMM0
 
     localparam BLOCK_0 = BLOCKSIZE - 1, BLOCK_1 = 2 * BLOCKSIZE -  1, BLOCK_2 = 3 * BLOCKSIZE - 1, BLOCK_3 = DATAWIDTH - 1; 
 
-    integer i;
+    integer i, j, k;
 
-    reg [BLOCKSIZE - 1:0] C [4];
-    reg [BLOCKSIZE - 1:0] T [7];
-    reg [BLOCKSIZE - 1:0] S [7];
-    reg [BLOCKSIZE - 1:0] M [7];
+    reg signed [BLOCKSIZE - 1:0] C [4];
+    reg signed [BLOCKSIZE - 1:0] T [7];
+    reg signed [BLOCKSIZE - 1:0] S [7];
+    reg signed [BLOCKSIZE - 1:0] M [7];
     
-    always @(A) begin
-       T[0] <= A[BLOCK_0 : 0] + A[BLOCK_3 : BLOCK_2 + 1];
-       T[1] <= A[BLOCK_2 : BLOCK_1 + 1] + A[BLOCK_3 : BLOCK_2 + 1];
-       T[2] <= A[BLOCK_0 : 0];
-       T[3] <= A[BLOCK_3 : BLOCK_2 + 1];
-       T[4] <= A[BLOCK_0 : 0] + A[BLOCK_1 : BLOCK_0 +  1];
-       T[5] <= A[BLOCK_2 : BLOCK_1 + 1] - A[BLOCK_0 : 0];
-       T[6] <= A[BLOCK_1 : BLOCK_0 + 1] - A[BLOCK_3 : BLOCK_2 + 1]; 
+    always @(posedge clk) begin
+        if (rst) for (i = 0; i < 7; i = i + 1) T[i] <= 'b0;
+        else if (load) begin
+            T[0] <= A[BLOCK_0 -: BLOCKSIZE] + A[BLOCK_3 -: BLOCKSIZE];
+            T[1] <= A[BLOCK_2 -: BLOCKSIZE] + A[BLOCK_3 -: BLOCKSIZE];
+            T[2] <= A[BLOCK_0 -: BLOCKSIZE];
+            T[3] <= A[BLOCK_3 -: BLOCKSIZE];
+            T[4] <= A[BLOCK_0 -: BLOCKSIZE] + A[BLOCK_1 -: BLOCKSIZE];
+            T[5] <= A[BLOCK_2 -: BLOCKSIZE] - A[BLOCK_0 -: BLOCKSIZE];
+            T[6] <= A[BLOCK_1 -: BLOCKSIZE] - A[BLOCK_3 -: BLOCKSIZE]; 
+        end
     end
 
-    always @(B) begin
-       S[0] <= B[BLOCK_0 : 0] + B[BLOCK_3 : BLOCK_2 + 1]; 
-       S[1] <= B[BLOCK_0 : 0];
-       S[2] <= B[BLOCK_1 : BLOCK_0 + 1] - B[BLOCK_3 : BLOCK_2 + 1];
-       S[3] <= B[BLOCK_2 : BLOCK_1 + 1] - B[BLOCK_0 : 0];
-       S[4] <= B[BLOCK_3 : BLOCK_2 + 1];
-       S[5] <= B[BLOCK_0 : 0] + B[BLOCK_1 : BLOCK_0 + 1];
-       S[6] <= B[BLOCK_2 : BLOCK_1 + 1] + B[BLOCK_3 : BLOCK_2 + 1];
+    always @(posedge clk) begin
+        if (rst) for (j = 0; j < 7; j = j + 1) S[j] <= 'b0;
+        else if(load) begin
+            S[0] <= B[BLOCK_0 : 0] + B[BLOCK_3 : BLOCK_2 + 1]; 
+            S[1] <= B[BLOCK_0 : 0];
+            S[2] <= B[BLOCK_1 : BLOCK_0 + 1] - B[BLOCK_3 : BLOCK_2 + 1];
+            S[3] <= B[BLOCK_2 : BLOCK_1 + 1] - B[BLOCK_0 : 0];
+            S[4] <= B[BLOCK_3 : BLOCK_2 + 1];
+            S[5] <= B[BLOCK_0 : 0] + B[BLOCK_1 : BLOCK_0 + 1];
+            S[6] <= B[BLOCK_2 : BLOCK_1 + 1] + B[BLOCK_3 : BLOCK_2 + 1];
+        end
     end
 
     always @(S or T) begin
-        for (i = 0; i < 7; i = i + 1) begin
-            M[i] <= T[i] * S[i];
+        for (k = 0; k < 7; k = k + 1) begin
+            M[k] <= T[k] * S[k];
         end
     end
     
@@ -53,11 +60,11 @@ module SMM0
        C[0] <= M[0] + M[3] - M[4] + M[6]; 
        C[1] <= M[2] + M[4]; 
        C[2] <= M[1] + M[3]; 
-       C[3] <= M[0] - M[1] + M[3] + M[5]; 
+       C[3] <= M[0] - M[1] + M[2] + M[5]; 
     end
 
 
-    always @(posedge clk or posedge rst) begin
+    always @(posedge clk) begin
         if (rst) C_out <= 'b0;
         else begin
             C_out <= {C[0], C[1], C[2], C[3]};
--- testbenches/SMM0_tb.cpp
+++ testbenches/SMM0_tb.cpp
@@ -4,13 +4,15 @@
 #include "verilated.h"
 #include <armadillo>
 
-
+#define N 2
+#define M N
 
 int main(int argc, char const *argv[])
 {
     arma::Mat<uint32_t> A = {{1,2}, {3,4}};
     arma::Mat<uint32_t> B = {{4,3}, {2,1}};
     arma::Mat<uint32_t> C(2, 2, arma::fill::zeros);
+    arma::Mat<uint32_t> C2(2, 2, arma::fill::zeros);
 
     std::random_device rnd_device;
     std::mt19937 mersenne_engine {rnd_device()};  // Generates random integers
@@ -30,8 +32,9 @@ int main(int argc, char const *argv[])
     smm0->B = {0, 0, 0, 0};
     smm0->clk = 0;
     smm0->rst = 0;
+    smm0->load  = 1;
 
-    while (contextp->time() < 10)
+    while (contextp->time() < 50)
     {
         contextp->timeInc(1);
         smm0->clk = !smm0->clk;
@@ -39,13 +42,28 @@ int main(int argc, char const *argv[])
         if (contextp->time() > 1 && contextp->time() < 4) smm0->rst = 1;
         else {
             smm0->rst = 0;
-            smm0->A = {A(0,0),A(0,1),A(1,0),A(1,1)};
-            smm0->B = {B(0,0),B(0,1),B(1,0),B(1,1)};
+            // smm0->A = {A(0,0),A(0,1),A(1,0),A(1,1)};
+            for (int i = 0; i < N; i++) 
+                for (int j = 0; j < M; j++)
+                    smm0->A.at(i * N + j) = A(i, j);
+            // smm0->B = {B(0,0),B(0,1),B(1,0),B(1,1)};
+            for (int i = 0; i < N; i++) 
+                for (int j = 0; j < M; j++)
+                    smm0->B.at(i * N + j) = B(i, j);
 
-            C = A * B;
-            std::cout << C << std::endl;
+            if (smm0->clk) { 
+                C = A * B;
+                std::cout << "C1: \n" << C << std::endl;
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j < M; j++) 
+                        C2(i, j)  = smm0->C_out.at((N * M - 1) - (i * N + j));
+
+                std::cout << "C2: \n" << C2 << std::endl;
+            }
+            std::cout << std::endl;
 
             A.for_each([&](arma::Mat<uint32_t>::elem_type &val){ val = dist(mersenne_engine); });
+            B.for_each([&](arma::Mat<uint32_t>::elem_type &val){ val = dist(mersenne_engine); });
 
         }
 
