993ddaf9dec4a41dd2baba92683d85a85f49b17b
rbrin <rbrinson2@gmail.com>
2025-06-03 18:15:59 -0400

    SMM0 matrix/vector tested and working

--- src/SMM0.v
+++ src/SMM0.v
@@ -1,3 +1,5 @@
+`define TEST 'b01
+
 
 module SMM0
 #(
@@ -7,7 +9,7 @@ module SMM0
 (
     input clk, rst,
     input signed [DATAWIDTH - 1:0] A, B,
-    input load,
+    input load, sel,
 
     output reg [DATAWIDTH - 1 :0] C_out
 
@@ -15,7 +17,7 @@ module SMM0
 
     localparam BLOCK_0 = BLOCKSIZE - 1, BLOCK_1 = 2 * BLOCKSIZE -  1, BLOCK_2 = 3 * BLOCKSIZE - 1, BLOCK_3 = DATAWIDTH - 1; 
 
-    integer i, j, k;
+    integer i, j;
 
     reg signed [BLOCKSIZE - 1:0] C [4];
     reg signed [BLOCKSIZE - 1:0] T [7];
@@ -25,49 +27,67 @@ module SMM0
     always @(posedge clk) begin
         if (rst) for (i = 0; i < 7; i = i + 1) T[i] <= 'b0;
         else if (load) begin
-            T[0] <= A[BLOCK_0 -: BLOCKSIZE] + A[BLOCK_3 -: BLOCKSIZE];
             T[1] <= A[BLOCK_2 -: BLOCKSIZE] + A[BLOCK_3 -: BLOCKSIZE];
             T[2] <= A[BLOCK_0 -: BLOCKSIZE];
             T[3] <= A[BLOCK_3 -: BLOCKSIZE];
             T[4] <= A[BLOCK_0 -: BLOCKSIZE] + A[BLOCK_1 -: BLOCKSIZE];
-            T[5] <= A[BLOCK_2 -: BLOCKSIZE] - A[BLOCK_0 -: BLOCKSIZE];
-            T[6] <= A[BLOCK_1 -: BLOCKSIZE] - A[BLOCK_3 -: BLOCKSIZE]; 
+
+            if (!sel) begin 
+                T[0] <= A[BLOCK_0 -: BLOCKSIZE] + A[BLOCK_3 -: BLOCKSIZE];
+                T[5] <= A[BLOCK_2 -: BLOCKSIZE] - A[BLOCK_0 -: BLOCKSIZE];
+                T[6] <= A[BLOCK_1 -: BLOCKSIZE] - A[BLOCK_3 -: BLOCKSIZE]; 
+            end
         end
     end
 
     always @(posedge clk) begin
         if (rst) for (j = 0; j < 7; j = j + 1) S[j] <= 'b0;
         else if(load) begin
-            S[0] <= B[BLOCK_0 : 0] + B[BLOCK_3 : BLOCK_2 + 1]; 
             S[1] <= B[BLOCK_0 : 0];
             S[2] <= B[BLOCK_1 : BLOCK_0 + 1] - B[BLOCK_3 : BLOCK_2 + 1];
             S[3] <= B[BLOCK_2 : BLOCK_1 + 1] - B[BLOCK_0 : 0];
             S[4] <= B[BLOCK_3 : BLOCK_2 + 1];
-            S[5] <= B[BLOCK_0 : 0] + B[BLOCK_1 : BLOCK_0 + 1];
-            S[6] <= B[BLOCK_2 : BLOCK_1 + 1] + B[BLOCK_3 : BLOCK_2 + 1];
+
+            if (!sel) begin
+                S[0] <= B[BLOCK_0 : 0] + B[BLOCK_3 : BLOCK_2 + 1]; 
+                S[5] <= B[BLOCK_0 : 0] + B[BLOCK_1 : BLOCK_0 + 1];
+                S[6] <= B[BLOCK_2 : BLOCK_1 + 1] + B[BLOCK_3 : BLOCK_2 + 1];
+            end
         end
     end
 
     always @(S or T) begin
-        for (k = 0; k < 7; k = k + 1) begin
-            M[k] <= T[k] * S[k];
+        M[1] <= T[1] * S[1];
+        M[2] <= T[2] * S[2];
+        M[3] <= T[3] * S[3];
+        M[4] <= T[4] * S[4];
+
+        if (!sel) begin
+            M[0] <= T[0] * S[0];
+            M[5] <= T[5] * S[5];
+            M[6] <= T[6] * S[6];
         end
+
     end
     
     
 
     always @(M) begin
-       C[0] <= M[0] + M[3] - M[4] + M[6]; 
-       C[1] <= M[2] + M[4]; 
-       C[2] <= M[1] + M[3]; 
-       C[3] <= M[0] - M[1] + M[2] + M[5]; 
+        C[1] <= M[2] + M[4]; 
+        C[2] <= M[1] + M[3]; 
+
+        if (!sel) begin
+            C[0] <= M[0] + M[3] - M[4] + M[6]; 
+            C[3] <= M[0] - M[1] + M[2] + M[5]; 
+        end
     end
 
 
     always @(posedge clk) begin
         if (rst) C_out <= 'b0;
         else begin
-            C_out <= {C[0], C[1], C[2], C[3]};
+            if (!sel)   C_out <= {C[0], C[1], C[2], C[3]};
+            else        C_out <= {C[1], 32'b0, C[2], 32'b0};
         end
     end
     
--- testbenches/SMM0_tb.cpp
+++ testbenches/SMM0_tb.cpp
@@ -7,10 +7,12 @@
 #define N 2
 #define M N
 
+
 int main(int argc, char const *argv[])
 {
     arma::Mat<uint32_t> A = {{1,2}, {3,4}};
     arma::Mat<uint32_t> B = {{4,3}, {2,1}};
+    arma::Col<uint32_t> B2(2, arma::fill::ones);
     arma::Mat<uint32_t> C(2, 2, arma::fill::zeros);
     arma::Mat<uint32_t> C2(2, 2, arma::fill::zeros);
 
@@ -32,20 +34,34 @@ int main(int argc, char const *argv[])
     smm0->B = {0, 0, 0, 0};
     smm0->clk = 0;
     smm0->rst = 0;
-    smm0->load  = 1;
+    smm0->load = 1;
+    smm0->sel = 0;
 
     while (contextp->time() < 50)
     {
         contextp->timeInc(1);
         smm0->clk = !smm0->clk;
 
+        // ---------- Reset Logic ---------- //
         if (contextp->time() > 1 && contextp->time() < 4) smm0->rst = 1;
-        else {
-            smm0->rst = 0;
+        else if (contextp->time() >= 25 && contextp->time() < 27) smm0->rst = 1;
+        else smm0->rst = 0;
+
+        // ---------- Load Logic ---------- //
+        if (contextp->time() > 17 && contextp->time() < 26) smm0->load = 0;
+        else if (contextp->time() > 40) smm0->load = 0;
+        else smm0->load = 1;
+
+        if (contextp->time() == 4) std::cout << "// ------------- Matrix x Matrix ------------- //" << std::endl;
+        if (contextp->time() == 25) std::cout << "// ------------- Matrix x Vector ------------- //" << std::endl;
+
+        // ------------- Matrix x Matrix ------------- //
+        if (contextp->time() < 25) {
             // smm0->A = {A(0,0),A(0,1),A(1,0),A(1,1)};
             for (int i = 0; i < N; i++) 
                 for (int j = 0; j < M; j++)
                     smm0->A.at(i * N + j) = A(i, j);
+
             // smm0->B = {B(0,0),B(0,1),B(1,0),B(1,1)};
             for (int i = 0; i < N; i++) 
                 for (int j = 0; j < M; j++)
@@ -53,18 +69,43 @@ int main(int argc, char const *argv[])
 
             if (smm0->clk) { 
                 C = A * B;
-                std::cout << "C1: \n" << C << std::endl;
                 for (int i = 0; i < N; i++)
                     for (int j = 0; j < M; j++) 
                         C2(i, j)  = smm0->C_out.at((N * M - 1) - (i * N + j));
 
                 std::cout << "C2: \n" << C2 << std::endl;
             }
-            std::cout << std::endl;
 
             A.for_each([&](arma::Mat<uint32_t>::elem_type &val){ val = dist(mersenne_engine); });
             B.for_each([&](arma::Mat<uint32_t>::elem_type &val){ val = dist(mersenne_engine); });
 
+        }
+
+        // ------------- Matrix x Vector ------------- //
+        else {
+            smm0->sel = 1;
+            // smm0->A = {A(0,0),A(0,1),A(1,0),A(1,1)};
+            for (int i = 0; i < N; i++) 
+                for (int j = 0; j < M; j++)
+                    smm0->A.at(i * N + j) = A(i, j);
+
+            // smm0->B = {B(0),B(0),B(1),B(1)};
+            for (int i = 0; i < N; i++) 
+                for (int j = 0; j < M; j++)
+                    smm0->B.at(i * N + j) = B2(i);
+
+            if (smm0->clk) { 
+                C = A * B2;
+                for (int i = 0; i < N; i++)
+                    for (int j = 0; j < M; j++) 
+                        C2(i, j)  = smm0->C_out.at((N * M - 1) - (i * N + j));
+
+                std::cout << "C2: \n" << C2 << std::endl;
+            }
+
+
+            A.for_each([&](arma::Mat<uint32_t>::elem_type &val){ val = dist(mersenne_engine); });
+            B2.for_each([&](arma::Col<uint32_t>::elem_type &val){ val = dist(mersenne_engine); });
         }
 
         smm0->eval();
